c0674c95a71b6d0a945bc06b1e5d9cbb03bdcef8
Geovani Celebrim <geovanicelebrim@gmail.com>
2015-06-15 00:35:29 -0300

    Correção na escolha e na alteração de canal

--- IC.sh
+++ IC.sh
@@ -1,71 +1,139 @@
 vetCanais="$(wl channels)"
 vetCanais=$(echo $vetCanais | tr " ")
 vetUso=""
+vetRuido=""
+vetNivel=""
 
-#Só é possível obter o ruido do canal atual.
-vetRuido="$(wl noise)"
-
-#Obtem medidas de dos canais-----------------------------------
-for c in $vetCanais; do
-	
-	#Mostra o canal que está sendo analizado
-	echo "Analizando canal ${c}"
-
-	#Obtem uso do canal
-	wl rm_req cca -c $c -d 50
-	sleep 1
-	logSystem="$(wl rm_rep)"
-	logSystem=$(echo $logSystem | tr ":""\n")
-
-	cont=0
-	for x in $logSystem
-	do
-		cont=$(($cont+1))
-		if test $cont = 29
-		then
-			clear
-			ocupacao=$x
-			vetUso="${vetUso} ${ocupacao}"
-		fi
-	done
-	clear
-
-	#Informa a ocupação do canal analizado.
-	echo "Ocupação: ${ocupacao} de 255."
-	echo "Mensagem completa: "
-	wl rm_rep
-
-	sleep 1
+#Obtém o uso do canal atual------------------------------------
+vetCanalAtual="$(wl channel)"
+vetCanalAtual=$(echo $vetCanalAtual | tr " ")
+for c in $vetCanalAtual; do
+	canalAtual=$c
 done
 
-#Fim---------------------------------------------------------
+wl rm_req cca -c $canalAtual -d 50
+sleep 1
+logSystem="$(wl rm_rep)"
+logSystem=$(echo $logSystem | tr ":""\n")
 
-#Calcula o melhor canal."Por enquanto só se baseando no uso, pois o ruido é obtido só do atual canal."------
-
-clear
 cont=0
-menorUso=256
-
-for c in $vetUso
+for x in $logSystem
 do
 	cont=$(($cont+1))
-	echo "Canal ${cont}, ocupação ${c}"
-	
-	if [ "$c" -lt "$menorUso" ]
+	if test $cont = 29
 	then
-		menorUso=$c
-		melhorCanal=$cont
+		ocupacaoAtual=$x
 	fi
 done
+#--------------------------------------------------------------
 
-echo "Melhor canal: ${melhorCanal} Uso: ${menorUso}"
+#Verifica se a ocupação está dentro da tolerância--------------
+clear
+tolerancia=30
 
-#Fim--------------------------------------------------------------------------------------------------------
+if [ $tolerancia -lt $ocupacaoAtual ]
+then
+	
+	#Obtem ocupação e o ruido de todos os canais---------------
+	for c in $vetCanais; do
+		clear
+		echo "Analizando canal ${c}"
+		#Obtem uso do canal
+		wl rm_req cca -c $c -d 50
+		sleep 1
+		logSystem="$(wl rm_rep)"
+		logSystem=$(echo $logSystem | tr ":""\n")
 
-#Altera o canal. "Alterar o canal implica em reiniciar o roteador, o que faz com que os dispositivos tenham que reconectar."-
+		cont=0
+		for x in $logSystem
+		do
+			cont=$(($cont+1))
+			if test $cont = 29
+			then
+				ocupacao=$x
+				vetUso="${vetUso} ${ocupacao}"
+			fi
+		done
+		clear
 
-wl down
-wl channel 6
-wl up
+		#Obtém o ruido do canal
+		wl rm_req rpi -c $c -d 50
+		sleep 1
+		logSystem="$(wl rm_rep)"
+		logSystem=$(echo $logSystem | tr ":""\n")
+		cont=0
+		for x in $logSystem
+		do
+			cont=$(($cont+1))
+			if test $cont = 30 || test $cont = 36 || test $cont = 42 || test $cont = 48 || test $cont = 54 || test $cont = 60 || test $cont = 66 || test $cont = 71
+			then
+				vetNivel="${vetNivel} ${x}"
+			fi
 
-#Fim-------------------------------------------------------------------------------------------------------------------------
+			if test $cont = 71
+			then
+				aux=87
+				ruido=0
+				for k in $vetNivel; do
+					ruido=$(($ruido+$aux*$k))
+					aux=$(($aux-5))
+				done
+				ruido=$(($ruido/255))
+				vetRuido="${vetRuido} ${ruido}"
+				vetNivel=""
+			fi
+
+		done
+	done
+
+
+	#Calcula o melhor canal.-------------------------------------
+
+	clear
+	cont=0
+	candidatos=""
+
+	#Seleciona os canais canditados.
+	for c in $vetUso
+	do
+		cont=$(($cont+1))
+		
+		if [ "$c" -lt "$ocupacaoAtual" ]
+		then
+			candidatos="${candidatos} ${cont}"
+		fi
+	done
+
+	#Elege o canal de acordo com o ruído.
+	eleito=1
+	ruido=256
+	melhorCanal=0
+
+	for c in $candidatos
+	do
+		cont=0
+		for x in $vetRuido
+		do
+			cont=$(($cont+1))
+			if test $cont = $c
+			then
+				if [ "$x" -lt "$ruido" ]
+				then
+					ruido=$x
+					melhorCanal=$cont
+				fi
+
+			fi
+		done
+	done
+	#-----------------------------------------------------------------------------
+
+	#Altera o canal---------------------------------------------------------------
+	if test $canalAtual != $melhorCanal
+	then
+		wl down;	wl channel $melhorCanal;	wl up;
+		echo "Canal anterior: ${canalAtual}. Novo canal: ${melhorCanal}."
+	fi
+
+	#Fim--------------------------------------------------------------------------
+fi
